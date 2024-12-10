9c42f7b4d960d74fe7df78715d85a1a063eb6ec9
Akil-Ansari-cmd <ansariakil0123@gmail.com>
2024-12-10 12:47:10 +0530

    Your commit message

--- src/Component/Header/Head4.jsx
+++ src/Component/Header/Head4.jsx
@@ -13,7 +13,8 @@ const Head4 = () => {
   }
   const Navigate = useNavigate();
   return (
-    <div className='flex flex-col text-white' onMouseEnter={handleOpen} onClick={() => Navigate("/Wish")}>
+    <>
+    <div className='xl:flex hidden flex-col text-white' onMouseEnter={handleOpen} onClick={() => Navigate("/Wish")}>
       <div className='lg:flex hidden text-sm font-normal'>0 Items</div>
       <div className='flex'>
         <div className='lg:flex hidden text-sm font-semibold whitespace-nowrap'>₹ 0</div>
@@ -29,6 +30,23 @@ const Head4 = () => {
         </div>
       }
     </div>
+    <div className='xl:hidden flex flex-col text-white' onClick={() => Navigate("/Wish")}>
+      <div className='lg:flex hidden text-sm font-normal'>0 Items</div>
+      <div className='flex'>
+        <div className='lg:flex hidden text-sm font-semibold whitespace-nowrap'>₹ 0</div>
+        <FiShoppingCart className='lg:w-5 lg:h-6 w-7 h-7 md:ml-8 ml-4  lg:-mt-2 -mt-0' />
+        <div className='lg:hidden md:flex hidden md:absolute ml-8 pl-1.5 font-semibold pt-0 bg-green-600 border rounded-full w-5 h-5  top-0 text-xs'>0</div>
+        <div className='md:hidden flex absolute ml-2  pl-1.5 font-semibold pt-0.5 bg-green-600 border rounded-full w-5 h-5  top-0.5 text-xs'>0</div>
+      </div>
+      {open &&
+        <div onMouseLeave={handleClose} className='absolute w-[25%] pb-3 top-12  rounded-b-full shadow-lg right-52 bg-white'>
+          <div onClick={(e) => e.stopPropagation} className='pt-5 text-center  text-black'>
+            Your Cart is empty
+          </div>
+        </div>
+      }
+    </div>
+    </>
   )
 }
 
